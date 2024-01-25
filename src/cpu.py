f7fa0902c20e7b51788a3d565e03a51c59eec501
unknown <2321759131@qq.com>
2024-01-25 19:48:16 +0800

    graphical lasso

--- src/cpu.py
+++ src/cpu.py
@@ -1,10 +1,95 @@
 import numpy as np
 from sklearn.cluster import KMeans
 import scanpy as sc
-import sklearn.covariance as skc
 import anndata
 import pandas as pd
+from scipy.optimize import minimize
+from sklearn import preprocessing
 
+
+class graphicalLasso:
+    '''
+        different from sklearn GraphicalLasso: rho is a matrix, not a integer
+    '''
+    def __init__(self, rho=None, maxItr=1e+3, tol=1e-2):
+        self.rho = rho
+        self.maxItr = int(maxItr)
+        self.tol = tol
+        self.scaler = None
+
+    def fit(self, X):
+        n_samples, n_features = X.shape[0], X.shape[1]
+
+        self.scaler = preprocessing.StandardScaler().fit(X)
+        self.X = self.scaler.transform(X)
+
+        S = self.X.T.dot(self.X) / n_samples
+
+        A = np.linalg.pinv(S)
+        A_old = A
+        invA = S
+
+        for i in range(self.maxItr):
+            for j in range(n_features):
+                R, s, sii = self.__get(S)
+                W = self.__get(invA)[0]
+                L = self.__get(A)[0]
+
+                sigma = sii + np.linalg.norm(self.rho)
+
+                U, D, V = np.linalg.svd(W)
+                W_half = U.dot(np.diag(np.sqrt(D)).dot(U.T))
+
+                b = np.linalg.pinv(W_half).dot(s)
+
+                beta = self.lasso(W_half, b, self.rho)
+
+                w = W.dot(beta)
+
+                l = -beta / (sigma - beta.T.dot(W).dot(beta))
+                lmbd = 1 / (sigma - beta.T.dot(W).dot(beta))
+
+                A = self.__put(L, l, lmbd)
+                invA = self.__put(W, w, sigma)
+                S = self.__put(R, s, sii)
+
+            if np.linalg.norm(A - A_old, ord=2) < self.tol:
+                break
+            else:
+                A_old = A
+
+        self.covariance = S
+        self.precision = A
+        return self
+
+    def __get(self, S):
+        end = S.shape[0] - 1
+        R = S[:-1, :-1]
+        s = S[end, :-1]
+        sii = S[end][end]
+
+        return [R, s, sii]
+
+    def __put(self, R, s, sii):
+        n = R.shape[0] + 1
+        X = np.empty([n, n])
+        X[1:, 1:] = R
+        X[1:, 0] = s
+        X[0, 1:] = s
+        X[0][0] = sii
+
+        return X
+
+    def lasso(self, W_half, b, rho):
+        def objective(beta):
+            return 0.5 * np.linalg.norm(W_half @ beta - b, 2) ** 2 + np.sum(np.abs(rho @ beta))
+
+        beta_init = np.zeros(W_half.shape[1])
+
+        res = minimize(objective, beta_init, method='SLSQP')
+        
+        return res.x
+    
 class cicero:
     '''
     parameters:
@@ -41,10 +126,6 @@ class cicero:
         sc.pp.normalize_total(self.group, target_sum=1e4)
         print('init done')
     
-    '''
-    parameters:
-        covmethod:str,{'empirical','graphical_lasso','ledoit_wolf','oas','shrunk','exact'},default='graphical lasso'
-        method of covariance estimation
-    '''
+    
     def runcicero(self,covmethod='graphical_lasso'):
-        
+        graphical=skc.GraphicalLasso()
